28ce9d5d15f1a0522398ef101144ff59534da02a
abrahameliaraque <abrahameliaraque@gmail.com>
2024-11-13 18:15:23 -0500

    I worked on functions today

---
+++ python_1/demoliendo_hoteles.py
@@ -0,0 +1,16 @@
+# Information about Abraham
+name = "Abraham"
+nickname = "veneko"
+age = 16
+bank_balance = 0.03  # Balance in dollars
+
+# Display the information
+def display_info():
+    print("User Information:")
+    print(f"Name: {name}")
+    print(f"Nickname: {nickname}")
+    print(f"Age: {age}")
+    print(f"Bank Account Balance: ${bank_balance:.2f}")
+
+# Run the function to show the info
+display_info()
