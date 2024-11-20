8e8c2a2dcea456b12edc9d81370a69280b82df56
abrahameliaraque <abrahameliaraque@gmail.com>
2024-11-20 16:54:47 -0500

    i made progres on Pompt Ingeneeing 1

--- python_1/demoliendo_hoteles.py
+++ python_1/demoliendo_hoteles.py
@@ -2,7 +2,17 @@
 name = "Abraham"
 nickname = "veneko"
 age = 16
-bank_balance = 0.03  # Balance in dollars
+bank_balance = 20 # Balance in dollars
+
+print("My name is: {Abraham}")
+# Your name
+name = "Abraham"
+# Use a loop to go through the letters of the name
+print("Letters in your name:")
+for letter in name:
+    print(letter)
+
+    
 
 # Display the information
 def display_info():
@@ -12,5 +22,14 @@ def display_info():
     print(f"Age: {age}")
     print(f"Bank Account Balance: ${bank_balance:.2f}")
 
+#making a decision depemnding on my bank balance
+
+if bank_balance < 10:
+    print("YOU ARE POOR!!!!!!!")
+
+elif bank_balance > 10:
+        print("You are still being poor but you got some money!")
+
 # Run the function to show the info
 display_info()
+
