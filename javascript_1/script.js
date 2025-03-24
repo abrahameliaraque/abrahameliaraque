0c2ba52352142ef6409ec3d38f4327f4dcd7635a
abrahameliaraque <abrahameliaraque@gmail.com>
2025-03-24 17:45:51 -0400

    I think that I had advanced on my javascript project

---
+++ javascript_1/script.js
@@ -0,0 +1,16 @@
+ alert ("Hola mi gente bella")
+
+ let age = 33;
+
+ const birthYear = 1900;
+
+ console.log("my birthday is ", age)
+
+ if (age >= 18) {
+
+console.log ("ve y consigue chamba")
+ }
+
+ else { 
+    console.log ("ve y termina tu primaria ")
+ }
